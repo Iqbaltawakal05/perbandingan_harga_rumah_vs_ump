b6576f2c655344934049062711ce6d51b9f6e2f0
Cahzello <rizkyrasya.m@gmail.com>
2026-01-23 01:44:11 +0700

    Add new CSV file for regional minimum wage data in Indonesia

--- Visulisasi/visual.py
+++ Visulisasi/visual.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import pandas as pd
+
+#baca data raw dan data ump
+df_rawdata = pd.read_csv("Data/raw/rumah123_raw.csv")
+df_ump = pd.read_csv("Data/support/Data_UMP.csv")
+df_kodeWilayah = pd.read_csv("Data/support/Data_KodeWilayah.csv", dtype={"kode": str})
+
+print("Kolom raw_data:", df_rawdata.columns)
+print("Kolom df_ump:", df_ump.columns)
+print("Kolom kode wilayah:", df_kodeWilayah.columns)
+
+print(df_kodeWilayah.dtypes)
