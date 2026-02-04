db7c4a973890d7ffc2b05e74d4f3285293088d3c
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2026-02-04 13:55:06 +0700

    Final web

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from Pages import visualisasi, gis, list_hargaRumah
+from Pages import gis
+from Pages import list_hargaRumah
+from Pages import visualisasi
 from streamlit_option_menu import option_menu
 import pandas as pd
 
