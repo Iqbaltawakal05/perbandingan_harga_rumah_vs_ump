d051674731ba909b156a1679504a728362a4cdff
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2026-02-04 15:23:01 +0700

    Final web

--- Cleaning/filterdata.py
+++ Cleaning/filterdata.py
@@ -34,9 +34,28 @@ df["price"] = (
 df = df[df["price"].str.contains(r"^Rp\s*\d", case=False, na=False)]
 
 df["price"] = df["price"].str.extract(
-    r"(Rp\s*\d+(?:\.\d+)?\s*(?:m|miliar|jt|juta))",
+    r"(Rp\s*\d+(?:\.\d+)?\s*(?:M|Miliar|Jt|Juta))",
     flags=re.IGNORECASE
 )[0]
+
+def normalize_price(price):
+    price = str(price).lower()
+
+    num = re.findall(r"\d+(?:\.\d+)?", price)
+    if not num:
+        return None
+
+    value = num[0]
+
+    if "m" in price or "miliar" in price:
+        return f"{value} M"
+    elif "jt" in price or "juta" in price:
+        return f"{value} Jt"
+    else:
+        return None
+    
+df["price"] = df["price"].apply(normalize_price)
+df = df.dropna(subset=["price"])
 
 df.to_csv(exportPath, index=False, encoding="utf-8-sig")
 
